91bc608085609d357d5755e915e77aa1e96cba54
Matthew Westenhaver <matthew.westenhaver@gmail.com>
2018-08-29 11:24:06 -0400

    navbar setup and themes imported

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
 
+import Navbar from './components/Navbar.jsx'
 import Home from './components/Home.jsx'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#232323',
+    },
+    secondary: {
+      main: '#999999',
+    },
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Route exact path='/' component={Home} />
-        </div>
-      </Router>
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <div>
+            <Navbar />
+            <div className="App">
+              <Route exact path='/' component={Home} />
+            </div>
+          </div>
+        </Router>
+      </MuiThemeProvider>
     );
   }
 }
